e5387cd2657b63fee52fdf804c276b3ec4d446a3
Abhishek Choudhary <abhishek.choudhary@prolifics.com>
2023-01-17 10:33:54 +0530

    developer cleaned

--- dep-qf/src/Environment.js
+++
@@ -1,2 +0,0 @@
-export const baseUrl = "http://10.11.12.149:8090"
-// export const baseUrl = "http://localhost:8090"
--- dep-qf/src/component/CustomComponent/ProgressPage.js
+++
@@ -1,6 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function ProgressPage () {
-
-    return<Typography align="center" variant="h4" > Screen Development is in Progress</Typography>
-}
