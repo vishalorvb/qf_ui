898da8d52990058110610ba1b292a5cfec32d6ed
Durgarao Akula <DAkula@prolifics.com>
2023-01-18 18:37:51 +0530

    created userpopup

---
+++ src/UsersPopups/AddUserPopup.js
@@ -0,0 +1,117 @@
+import React, { useEffect, useRef, useState } from 'react'
+// import axios from 'axios';
+import { Autocomplete, Button, Container, Dialog, DialogActions, DialogContent, DialogTitle, Grid, IconButton, Typography } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
+import SaveIcon from '@mui/icons-material/Save';
+
+function AddUserPopup(props) {
+
+    const { open, setOpen, users} = props;
+
+    let first_name = useRef();
+    let last_name = useRef();
+    let email = useRef();
+    let password = useRef();
+    let uid = useRef();
+    let roleId = useRef();
+    const [roleObject, setRoleObject] = useState([]);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const getUserRoles = () => {
+        // axios
+        //     .get(baseUrl + `/OrganisationMS/Users/getAllRoles`)
+        //     .then((Response) => {
+        //         var response = Response.data;
+        //         setRoleObject(response);
+        //     })
+        //     .catch((error) => {
+        //         console.log(error)
+        //     });
+    };
+
+    useEffect(() => {
+        getUserRoles();
+    }, []);
+
+    const submit = (e) => {
+    }
+
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth='md'>
+            <DialogTitle id="alert-dialog-title" className="dialogTitle border-bottom" sx={{
+                padding: 0.5,
+                backgroundColor: "rgba(137,196,244,1)",
+            }}>
+                <Grid container direction="row" justify="space-between" alignItems="center" className="poptitle">
+                    <Typography sx={{ marginLeft: 1, marginTop: "auto", marginBottom: "auto " }} variant="inherit">Add User </Typography>
+                    <IconButton sx={{ marginLeft: "auto" }} onClick={handleClose} className="btn-close ">
+                        <ClearIcon sx={{ color: 'white' }} />
+                    </IconButton>
+                </Grid>
+            </DialogTitle>
+            <DialogContent className="AddUsers" style={{ marginTop: "10px", marginLeft: "auto", marginRight: "auto" }}>
+                <div >
+                    <form>
+                        <div>
+                            <Container component={'div'} sx={{ display: "flex", flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'flex-start' }} >
+                                <Grid container item xs={12} sm={8} md={6} sx={{ marginBottom: '10px' }} >
+                                    <Grid item xs={6} sm={6} md={3}><label>First Name <span className="importantfield" >*</span>:</label></Grid>
+                                    <Grid item xs={6} sm={6} md={8}> 
+                                    <input type="text" ref={first_name} name="" placeholder="Enter First Name" />
+                                    </Grid>
+                                </Grid>
+                                <Grid container item xs={12} sm={8} md={6} sx={{ marginBottom: '10px' }} >
+                                    <Grid item xs={6} sm={6} md={3}><label>Last Name <span className="importantfield" >*</span>:</label></Grid>
+                                    <Grid item xs={6} sm={6} md={8}> <input type="text" ref={last_name} name="" placeholder="Enter Last Name" /></Grid>
+                                </Grid>
+                                <Grid container item xs={12} sm={8} md={6} sx={{ marginBottom: '10px' }} >
+                                    <Grid item xs={6} sm={6} md={3}><label>User Id <span className="importantfield" >*</span>:</label></Grid>
+                                    <Grid item xs={6} sm={6} md={8}> <input type="text" ref={uid} name="" placeholder="Enter 4 digit number only"/></Grid>
+                                </Grid>
+                                <Grid container item xs={12} sm={8} md={6} sx={{ marginBottom: '10px' }} >
+                                    <Grid item xs={6} sm={6} md={3}><label>Password <span className="importantfield" >*</span>:</label></Grid>
+                                    <Grid item xs={6} sm={6} md={8}><input type="text" ref={password} name="" placeholder="Enter atleast 8 Characters"/></Grid>
+                                </Grid>
+                                <Grid container item xs={12} sm={8} md={6} sx={{ marginBottom: '10px' }} >
+                                    <Grid item xs={6} sm={6} md={3}><label>Email Id <span className="importantfield" >*</span>:</label></Grid>
+                                    <Grid item xs={6} sm={6} md={8}> <input name="email" type="text" ref={email} placeholder="Enter Email"/></Grid>
+                                </Grid>
+                                <Grid container item xs={12} sm={8} md={6} sx={{ marginBottom: '10px' }} >
+                                    <Grid item xs={6} sm={6} md={3}><label>Role  <span className="importantfield" >*</span>:</label></Grid>
+                                    <Grid item xs={6} sm={6} md={8}>
+                                        <Autocomplete
+                                            // ref={roleId}
+                                            size="small"
+                                            options={roleObject}
+                                            onChange={(e, value) => {
+                                                roleId.current = value.id
+                                            }}
+                                            // onSelect={onRoleSelect}
+                                            getOptionLabel={(option) => option.type}
+                                            noOptionsText={'Role not found'}
+                                            renderInput={(params) =>
+                                                <div ref={params.InputProps.ref}>
+                                                <input type="text" name="roleAutocomplete" {...params.inputProps} placeholder="Enter atleast 3 letters" />
+                                              </div>
+                                            }
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </Container>
+                        </div>
+                    </form>
+                </div>
+            </DialogContent>
+            <DialogActions style={{ marginTop: "1px", marginBottom: "5px", marginLeft: "auto", marginRight: "auto" }}>
+                <Button variant="contained" onClick={submit} startIcon={<SaveIcon />}>
+                    Save
+                </Button>
+            </DialogActions>
+        </Dialog>
+  )
+}
+
+export default AddUserPopup
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,13 +1,148 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useHead from "../hooks/useHead";
+import { Button, IconButton, Tooltip } from "@mui/material";
+import Table from "../CustomComponent/Table";
+import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddUserPopup from '../UsersPopups/AddUserPopup';
+import EditUserPopup from '../UsersPopups/EditUserPopup';
+import DeleteUserPopup from '../UsersPopups/DeleteUserPopup';
+// import ActiveUserPopup from './ActiveUserPopup';
+// import DeactiveUserPopup from './DeactiveUserPopup';
+import PersonOutlineTwoToneIcon from '@mui/icons-material/PersonOutlineTwoTone';
+import PersonOffTwoToneIcon from '@mui/icons-material/PersonOffTwoTone';
+// import axios from 'axios';
 
 export default function Admin() {
   const { setHeader } = useHead();
+  const [open, setOpen] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
+  const [openDelete, setOpenDelete] = useState(false);
+  const [openActive, setOpenActive] = useState(false);
+  const [openDeactive, setOpenDeactive] = useState(false);
+  const [editObject, setEditObject] = useState([]);
+  const [deleteObject, setDeleteObject] = useState([]);
+  const [activeObject, setActiveObject] = useState([]);
+  const [deactiveObject, setDeactiveObject] = useState([]);
+
+  const addUserHandler = () => {
+    setOpen(true);
+  }
+
+  const editUserHandler = (e) => {
+    setOpenEdit(true);
+    setEditObject(e);
+  }
+
+  const deleteUserHandler = (e) => {
+    setOpenDelete(true);
+    setDeleteObject(e);
+  }
+
+  const activateUserHandler = (e) => {
+    setOpenActive(true);
+    setActiveObject(e);
+  }
+
+  const deactivateUserHandler = (e) => {
+    setOpenDeactive(true);
+    setDeactiveObject(e);
+  }
 
   useEffect(() => {
     setHeader((ps) => {
       return { ...ps, name: "Admin" };
     });
   }, []);
-  return <>Admin</>;
+
+  const users = [
+    { "id":"100", "fname": "Durgarao", "lname": "Akula", "email": "durgarao@gmail.com", "sid": 65 },
+    { "id":"101", "fname": "Abhishek", "lname": "ch", "email": "abhishek@gmail.com", "sid": 72 },
+    { "id":"102", "fname": "Vishal", "lname": "kumar", "email": "vishal@gmail.com", "sid": 79 },
+  ];
+
+  const columns = [
+    { headerName: "S.No",field:'sno' ,valueGetter: (index) => index.api.getRowIndex(index.row.id) + 1, flex: 1, headerAlign: "center", sortable: false, align: 'center' },
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 3,
+      headerAlign: "center",
+      sortable: false,
+      align: 'left',
+      renderCell: (params) => {
+        return (
+          <div>
+            {params.row.fname +" "+ params.row.lname}
+          </div>
+        )
+      }
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      flex: 3,
+      headerAlign: "center",
+      sortable: false,
+      align: 'left'
+
+    },
+    {
+      field: 'sid',
+      headerName: 'User Id',
+      flex: 3,
+      headerAlign: "center",
+      sortable: false,
+      align: 'left'
+    },
+    {
+      field: '',
+      headerName: 'Actions',
+      flex: 3,
+      sortable: false,
+      renderCell: (param) => {
+        return (
+          <>
+            <Tooltip title="Edit">
+              <IconButton onClick={(e) => { editUserHandler(param.row) }}><EditIcon ></EditIcon></IconButton>
+            </Tooltip>
+            <Tooltip title="Delete">
+              <IconButton onClick={(e) => { deleteUserHandler(param.row) }}><DeleteIcon ></DeleteIcon></IconButton>
+            </Tooltip>
+            {param.row.status === 0 ?
+              <Tooltip title="Inactive">
+                <IconButton onClick={(e) => { activateUserHandler(param.row) }}><PersonOffTwoToneIcon ></PersonOffTwoToneIcon></IconButton>
+              </Tooltip> :
+              <Tooltip title="Active">
+                <IconButton onClick={(e) => { deactivateUserHandler(param.row) }}><PersonOutlineTwoToneIcon ></PersonOutlineTwoToneIcon></IconButton>
+              </Tooltip>
+            }
+          </>
+        )
+      },
+      headerAlign: "center",
+      align: 'center',
+    },
+  ];
+
+  return (
+    <div>
+      <div className="recenttable" style={{ flot: "right", marginBottom: "10px" }}>
+        <Button variant="contained" endIcon={<AddIcon />} onClick={addUserHandler}>Add User</Button>
+      </div>
+      <div className="datatable" style={{ marginTop: "20px" }}>
+        {open ? <AddUserPopup open={open} setOpen={setOpen}  users={users}  /> : ""}
+        {openEdit ? <EditUserPopup object={editObject} openEdit={openEdit} setOpenEdit={setOpenEdit}  /> : ""}
+        {openDelete ? <DeleteUserPopup object={deleteObject} openDelete={openDelete} setOpenDelete={setOpenDelete} /> : ""}
+        {/* {openActive ? <ActiveUserPopup object={activeObject} openActive={openActive} setOpenActive={setOpenActive} getUserDetails={getUserDetails} setActSuccessMsg={setActSuccessMsg} /> : ""}
+        {openDeactive ? <DeactiveUserPopup object={deactiveObject} openDeactive={openDeactive} setOpenDeactive={setOpenDeactive} getUserDetails={getUserDetails} setDeactSuccessMsg={setDeactSuccessMsg} /> : ""}  */}
+        <Table
+          columns={columns}
+          rows={users}
+          // hidefooter={false}
+        />
+      </div>
+    </div>
+  )
 }
