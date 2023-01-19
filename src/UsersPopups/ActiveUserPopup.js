33f35209eb80bb0e424121987ba9b9b8acc28d8c
Durgarao Akula <DAkula@prolifics.com>
2023-01-19 12:10:31 +0530

    committed admin, active and deactive

---
+++ src/UsersPopups/ActiveUserPopup.js
@@ -0,0 +1,53 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid,IconButton, Typography } from '@mui/material'
+import React from 'react';
+import ClearIcon from '@mui/icons-material/Clear';
+import PersonOutlineTwoToneIcon from '@mui/icons-material/PersonOutlineTwoTone';
+// import axios from 'axios';
+
+function ActiveUserPopup(props) {
+
+    const { openActive, setOpenActive, object } = props;
+    const  user  = object.fname+" "+object.lname;
+
+    const handleClose = () => {
+        setOpenActive(false);
+    };
+
+    const submit = () => {
+
+    }
+
+    return (
+        <div className="border" style={{ marginBottom: "20px", marginTop: "20px" }}>
+            <Dialog open={openActive} onClose={handleClose} style={{ marginLeft: "15px", marginTop: "20px" }}>
+                <DialogTitle id="alert-dialog-title" className="dialogTitle border-bottom" sx={{
+                    padding: 0.5,
+                    backgroundColor: "rgba(137,196,244,1)",
+                }}>
+                    <Grid container direction="row" justify="space-between" alignItems="center" className="poptitle">
+                        <Typography sx={{ marginLeft: 1 }} variant="inherit">Activate User </Typography>
+                        <IconButton sx={{ marginLeft: "auto" }} onClick={handleClose} className="btn-close ">
+                            <ClearIcon sx={{ color: 'white' }} />
+                        </IconButton>
+                    </Grid>
+                </DialogTitle>
+                <DialogContent className="DeleteUsers" style={{ marginTop: "15px", marginLeft: "auto", marginRight: "auto" }}>
+                    <div>
+                        <form>
+                            <div>
+                                <span>Are you sure you want to Activate User {user}</span>
+                            </div>
+                        </form>
+                    </div>
+                </DialogContent>
+                <DialogActions style={{ marginTop: "5px", marginBottom: "5px", marginLeft: "auto", marginRight: "auto" }}>
+                    <Button variant="contained" onClick={submit} startIcon={<PersonOutlineTwoToneIcon />}>
+                        Activate
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    )
+}
+
+export default ActiveUserPopup
