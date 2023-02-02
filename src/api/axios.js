52f265a46d39aa5ff9ec4528f95adf02239e4196
Abhishek Choudhary <abhishek.choudhary@prolifics.com>
2023-02-02 16:00:29 +0530

    logout init

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 const BASE_URL = "http://10.11.12.240";
 
+export default axios.create({
+  baseURL: BASE_URL,
+});
+
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
