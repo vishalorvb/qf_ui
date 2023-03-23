c26fefdffc4d954d7be1e6b8f125468b076d0d28
Abhishek Choudhary <abhishek.choudhary@prolifics.com>
2023-03-23 12:03:43 +0530

    admin imports fixed

--- src/Components/UsersPopups/ActiveUserPopup.js
+++ src/Components/UsersPopups/ActiveUserPopup.js
@@ -1,69 +1,114 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid,IconButton, Typography } from '@mui/material'
-import React from 'react';
-import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import useAuth from "../hooks/useAuth";
-import useAxios from "../hooks/useAxios";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import useAuth from "../../hooks/useAuth";
+import useAxios from "../../hooks/useAxios";
 // import axios from 'axios';
 
 function ActiveUserPopup(props) {
+  const { openActive, setOpenActive, object, getUsers, setActSuccessMsg } =
+    props;
+  const user = object.firstName + " " + object.lastName;
+  const axiosPrivate = useAxios();
+  const { auth } = useAuth();
+  const loggedInId = auth.info.id;
+  const id = object.id;
 
-    const { openActive, setOpenActive, object, getUsers, setActSuccessMsg } = props;
-    const  user  = object.firstName+" "+object.lastName;
-    const axiosPrivate = useAxios();
-    const { auth } = useAuth();
-    const loggedInId = auth.info.id;
-    const id = object.id;
+  const handleClose = () => {
+    setOpenActive(false);
+  };
 
-    const handleClose = () => {
-        setOpenActive(false);
-    };
+  const submit = () => {
+    axiosPrivate
+      .post(
+        `qfauthservice/user/UpdateUserStatus?current_user_id=${loggedInId}&user_id=${id}&user_status=1`
+      )
+      .then((res) => {
+        console.log(res.message);
+        setActSuccessMsg(true);
+        getUsers();
+        setTimeout(() => {
+          setActSuccessMsg(false);
+        }, 3000);
+      });
+    handleClose();
+  };
 
-    const submit = () => {
-        axiosPrivate.post(`qfauthservice/user/UpdateUserStatus?current_user_id=${loggedInId}&user_id=${id}&user_status=1`).then(
-            res => {
-                console.log(res.message);
-                setActSuccessMsg(true);
-                getUsers();
-                setTimeout(() => {
-                    setActSuccessMsg(false)
-                }, 3000);
-            }
-        )
-        handleClose();
-    }
-
-    return (
-        <div className="border" style={{ marginBottom: "20px", marginTop: "20px" }}>
-            <Dialog open={openActive} onClose={handleClose} style={{ marginLeft: "15px", marginTop: "20px" }}>
-                <DialogTitle id="alert-dialog-title" className="dialogTitle border-bottom" sx={{
-                    padding: 0.5,
-                    backgroundColor: "rgba(137,196,244,1)",
-                }}>
-                    <Grid container direction="row" justify="space-between" alignItems="center" className="poptitle">
-                        <Typography sx={{ marginLeft: 1 }} variant="inherit">Activate User </Typography>
-                        <IconButton sx={{ marginLeft: "auto" }} onClick={handleClose} className="btn-close ">
-                            <ClearOutlinedIcon sx={{ color: 'white' }} />
-                        </IconButton>
-                    </Grid>
-                </DialogTitle>
-                <DialogContent className="DeleteUsers" style={{ marginTop: "15px", marginLeft: "auto", marginRight: "auto" }}>
-                    <div>
-                        <form>
-                            <div>
-                                <span>Are you sure you want to Activate User {user}</span>
-                            </div>
-                        </form>
-                    </div>
-                </DialogContent>
-                <DialogActions style={{ marginTop: "5px", marginBottom: "5px", marginLeft: "auto", marginRight: "auto" }}>
-                    <Button variant="contained" onClick={submit} startIcon={<PersonOutlineOutlinedIcon />}>
-                        Activate
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    )
+  return (
+    <div className="border" style={{ marginBottom: "20px", marginTop: "20px" }}>
+      <Dialog
+        open={openActive}
+        onClose={handleClose}
+        style={{ marginLeft: "15px", marginTop: "20px" }}
+      >
+        <DialogTitle
+          id="alert-dialog-title"
+          className="dialogTitle border-bottom"
+          sx={{
+            padding: 0.5,
+            backgroundColor: "rgba(137,196,244,1)",
+          }}
+        >
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+            className="poptitle"
+          >
+            <Typography sx={{ marginLeft: 1 }} variant="inherit">
+              Activate User{" "}
+            </Typography>
+            <IconButton
+              sx={{ marginLeft: "auto" }}
+              onClick={handleClose}
+              className="btn-close "
+            >
+              <ClearOutlinedIcon sx={{ color: "white" }} />
+            </IconButton>
+          </Grid>
+        </DialogTitle>
+        <DialogContent
+          className="DeleteUsers"
+          style={{ marginTop: "15px", marginLeft: "auto", marginRight: "auto" }}
+        >
+          <div>
+            <form>
+              <div>
+                <span>Are you sure you want to Activate User {user}</span>
+              </div>
+            </form>
+          </div>
+        </DialogContent>
+        <DialogActions
+          style={{
+            marginTop: "5px",
+            marginBottom: "5px",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          <Button
+            variant="contained"
+            onClick={submit}
+            startIcon={<PersonOutlineOutlinedIcon />}
+          >
+            Activate
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 
-export default ActiveUserPopup
+export default ActiveUserPopup;
--- src/Components/UsersPopups/DeactiveUserPopup.js
+++ src/Components/UsersPopups/DeactiveUserPopup.js
@@ -1,69 +1,118 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, IconButton, Typography } from '@mui/material'
-import React from 'react';
-import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
-import PersonOffOutlinedIcon from '@mui/icons-material/PersonOffOutlined';
-import useAuth from "../hooks/useAuth";
-import useAxios from "../hooks/useAxios";
-// import axios from 'axios';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
+import PersonOffOutlinedIcon from "@mui/icons-material/PersonOffOutlined";
+import useAuth from "../../hooks/useAuth";
+import useAxios from "../../hooks/useAxios";
 
 function DeactiveUserPopup(props) {
+  const {
+    openDeactive,
+    setOpenDeactive,
+    object,
+    getUsers,
+    setDeactSuccessMsg,
+  } = props;
+  const user = object.firstName + " " + object.lastName;
+  const id = object.id;
+  const axiosPrivate = useAxios();
+  const { auth } = useAuth();
+  const loggedInId = auth.info.id;
 
-    const { openDeactive, setOpenDeactive, object, getUsers, setDeactSuccessMsg } = props;
-    const user = object.firstName + " " + object.lastName;
-    const id = object.id;
-    const axiosPrivate = useAxios();
-    const { auth } = useAuth();
-    const loggedInId = auth.info.id;
+  const handleClose = () => {
+    setOpenDeactive(false);
+  };
 
-    const handleClose = () => {
-        setOpenDeactive(false);
-    };
+  const submit = () => {
+    axiosPrivate
+      .post(
+        `qfauthservice/user/UpdateUserStatus?current_user_id=${loggedInId}&user_id=${id}&user_status=0`
+      )
+      .then((res) => {
+        console.log(res.message);
+        setDeactSuccessMsg(true);
+        getUsers();
+        setTimeout(() => {
+          setDeactSuccessMsg(false);
+        }, 3000);
+      });
+    handleClose();
+  };
 
-    const submit = () => {
-        axiosPrivate.post(`qfauthservice/user/UpdateUserStatus?current_user_id=${loggedInId}&user_id=${id}&user_status=0`).then(
-            res => {
-                console.log(res.message);
-                setDeactSuccessMsg(true);
-                getUsers();
-                setTimeout(() => {
-                    setDeactSuccessMsg(false)
-                }, 3000);
-            }
-        )
-        handleClose();
-    }
-
-    return (
-        <div className="border" style={{ marginBottom: "20px", marginTop: "20px" }}>
-            <Dialog open={openDeactive} onClose={handleClose} style={{ marginLeft: "15px", marginTop: "20px" }}>
-                <DialogTitle id="alert-dialog-title" className="dialogTitle border-bottom" sx={{
-                    padding: 0.5,
-                    backgroundColor: "rgba(137,196,244,1)",
-                }}>
-                    <Grid container direction="row" justify="space-between" alignItems="center" className="poptitle">
-                        <Typography sx={{ marginLeft: 1 }} variant="inherit">Deactivate User </Typography>
-                        <IconButton sx={{ marginLeft: "auto" }} onClick={handleClose} className="btn-close ">
-                            <ClearOutlinedIcon sx={{ color: 'white' }} />
-                        </IconButton>
-                    </Grid>
-                </DialogTitle>
-                <DialogContent className="DeleteUsers" style={{ marginTop: "15px", marginLeft: "auto", marginRight: "auto" }}>
-                    <div>
-                        <form>
-                            <div>
-                                <span>Are you sure you want to Deactivate User {user}</span>
-                            </div>
-                        </form>
-                    </div>
-                </DialogContent>
-                <DialogActions style={{ marginTop: "5px", marginBottom: "5px", marginLeft: "auto", marginRight: "auto" }}>
-                    <Button variant="contained" onClick={submit} startIcon={<PersonOffOutlinedIcon />}>
-                        Deactivate
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    )
+  return (
+    <div className="border" style={{ marginBottom: "20px", marginTop: "20px" }}>
+      <Dialog
+        open={openDeactive}
+        onClose={handleClose}
+        style={{ marginLeft: "15px", marginTop: "20px" }}
+      >
+        <DialogTitle
+          id="alert-dialog-title"
+          className="dialogTitle border-bottom"
+          sx={{
+            padding: 0.5,
+            backgroundColor: "rgba(137,196,244,1)",
+          }}
+        >
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+            className="poptitle"
+          >
+            <Typography sx={{ marginLeft: 1 }} variant="inherit">
+              Deactivate User{" "}
+            </Typography>
+            <IconButton
+              sx={{ marginLeft: "auto" }}
+              onClick={handleClose}
+              className="btn-close "
+            >
+              <ClearOutlinedIcon sx={{ color: "white" }} />
+            </IconButton>
+          </Grid>
+        </DialogTitle>
+        <DialogContent
+          className="DeleteUsers"
+          style={{ marginTop: "15px", marginLeft: "auto", marginRight: "auto" }}
+        >
+          <div>
+            <form>
+              <div>
+                <span>Are you sure you want to Deactivate User {user}</span>
+              </div>
+            </form>
+          </div>
+        </DialogContent>
+        <DialogActions
+          style={{
+            marginTop: "5px",
+            marginBottom: "5px",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          <Button
+            variant="contained"
+            onClick={submit}
+            startIcon={<PersonOffOutlinedIcon />}
+          >
+            Deactivate
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 
-export default DeactiveUserPopup
+export default DeactiveUserPopup;
