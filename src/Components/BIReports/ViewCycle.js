2eb1aaa0ce9041bb029f01fc884393cec2345dae
Lavanya Muthyala <LMuthyala@prolifics.com>
2023-05-18 10:37:43 +0530

    Cycles modified

--- src/Components/BIReports/ViewCycle.js
+++ src/Components/BIReports/ViewCycle.js
@@ -1,6 +1,7 @@
 import { Button, Grid, Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import axios from "../../api/axios";
+let list = []
 
 
 
@@ -16,44 +17,74 @@ export default function ViewCycle({selectedCycleList }) {
   const [failedTestcases, setFailedTestcases] = useState([]);
   const [passedTestcases, setPassedTestcases] = useState();
   const [defectsTestcases, setDefectsTestcases] = useState();
-
-
-  
-  
-  
-  
-
-
-
-
+  const [id,setId] = useState()
+  const [formData, setFormData] = useState([]);
+  const [reportData,setReportData] = useState([])
   const getCyclesReports = () => {
+   
     axios.get("/Biservice/projects/93/cycles/17").then((resp) => {
       console.log(resp.data.info.selectedcycles);
       setSelectedCycleReports(resp?.data?.info?.selectedcycles);
+      // setReportData(resp?.data?.info?.selectedcycles?.reports)
+      (resp?.data?.info?.selectedcycles?.reports).forEach((r)=>
+      {
+        console.log(r)
+        let temp ={}
+        temp["id"] = r.id;
+        temp["failed_testcases"]=r.failed_testcases;
+        temp["passed_testcases"] = r.passed_testcases;
+        temp["defects_testcases"] = r.defects_testcases
+        // console.log(temp)
+        list.push(temp)
+      })
+      console.log(list)
     });
   };
-
-
-
  
-
-
-
-
   useEffect(() => {
     setSelectedCycleInfo(selectedCycleList);
     getCyclesReports();
+    
   }, [selectedCycleList])
 
- 
+ console.log(list)
 console.log(failedTestcases)
-
+console.log(selectedCycleReports)
+console.log(reportData)
+const handleInputChange = (id, fieldName, value) => {
+  console.log(id)
+  // setFormData(prevFormData => {
+  //   const updatedFormData = [...prevFormData];
+  //   if (updatedFormData[index]) {
+  //     updatedFormData[index] = { ...updatedFormData[index], [fieldName]: value };
+  //   } else {
+  //     updatedFormData[index] = { [fieldName]: value };
+  //   }
+  //   return updatedFormData;
+  // });
+  list.forEach((r)=>
+  {
+    if(r.id == id)
+    {
+      r[fieldName] = value;
+      r[fieldName]= value;
+      r[fieldName] = value;
+    }
+    // finalData.push(r)
+  })
+  console.log(list)
+};
+const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log(formData);
+  // Here you can perform further actions with the formData
+};
   return (
     <>
       <Grid item md={12}>
         <Typography variant="h4">Details</Typography>
       </Grid>
-      <form  >
+      <form  onSubmit={handleSubmit}>
         <Stack spacing={1} mt={1}>
           <lable>Cycle Name :</lable>
           <input
@@ -75,7 +106,7 @@ console.log(failedTestcases)
 
           {selectedCycleReports?.reports?.map((testset) => (
             <Grid container direction="row" spacing={1} alignItems="center" mt={0.5}>
-              <Grid item md={2.5}>
+              <Grid item md={2.5} >
                 <input
                 type="text"
                   defaultValue={testset.testset_name}
@@ -86,14 +117,21 @@ console.log(failedTestcases)
               <Grid item md={1}>
                 <input
                   defaultValue={testset.failed_testcases}
-                  onChange={(e) => { }}
+                  // onChange={(e) => { 
+                  //   setFailedTestcases(e.target.value)
+                    
+                  // }}
+                  onChange={(e) => {
+                    console.log(testset)
+                    handleInputChange(testset.id,'failed_testcases', e.target.value)}}
                 />
               </Grid>
 
               <Grid item md={1}>
                 <input
                   defaultValue={testset.passed_testcases}
-                  onChange={(e) => { setPassedTestcases(e.target.value) }}
+                  // onChange={(e) => { setPassedTestcases(e.target.value) }}
+                  onChange={(e) => handleInputChange(testset.id,'passed_testcases', e.target.value)}
 
                 />
               </Grid>
@@ -101,8 +139,8 @@ console.log(failedTestcases)
               <Grid item md={1}>
                 <input
                   defaultValue={testset.defects_testcases}
-                  onChange={(e) => { setDefectsTestcases(e.target.value) }}
-
+                  // onChange={(e) => { setDefectsTestcases(e.target.value) }}
+                  onChange={(e) => handleInputChange(testset.id,'defects_testcases', e.target.value)}
                 />
               </Grid>
             </Grid> 
