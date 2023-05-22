29c6a1722a7c92fba5f00e093ff2fc7905968625
Vikas Yadav <VYadav@prolifics.com>
2023-05-22 16:45:18 +0530

    bugs related to project order of  screen elements reports icon

--- src/Components/Reports/ReportDetails.js
+++ src/Components/Reports/ReportDetails.js
@@ -8,6 +8,7 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import TouchAppIcon from '@mui/icons-material/TouchApp';
 import CancelIcon from '@mui/icons-material/Cancel';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import InfoIcon from '@mui/icons-material/Info';
 import moment from "moment";
 import { Padding } from '@mui/icons-material';
 
@@ -43,7 +44,7 @@ export default function ReportDetails({ selectedItemData }) {
             <Divider></Divider>
 
             {selectedItemData?.datasetdata?.map((dataset) => {
-                console.log(dataset?.result_type)
+                console.log(dataset)
                 return (<div>
                     {dataset?.result_type === "screen" && <Typography mb={0} mt={1} p={0.7} sx={{ color: "#5C6780", fontSize: "16px", backgroundColor: "#e8f2fd", borderRadius: "5px" }} variant='h6'>{dataset?.text}</Typography>}
                     <Grid>
@@ -51,10 +52,11 @@ export default function ReportDetails({ selectedItemData }) {
                             {dataset?.text.includes("Entered User") === true ? <KeyboardIcon sx={{ color: "#636161", marginRight: "10px" }} />
                                 : dataset?.text.includes("Entered Password") === true ? <VisibilityOffIcon sx={{ color: "#636161", marginRight: "10px" }} />
                                     : dataset?.text.includes("Text is displayed as :") === true ? <CheckCircleIcon sx={{ color: "green", marginRight: "10px" }} />
+                                    : dataset?.text.includes("Clicked on") === true ? <TouchAppIcon sx={{ color: "#636161", marginRight: "10px" }} />
                                         : <KeyboardIcon sx={{ color: "#636161", marginRight: "10px" }} />}<Typography variant='p'>{dataset?.text}</Typography></div>}
                     </Grid>
                     <Grid>
-                        {dataset?.result_type === "info" && <div style={{ marginTop: "12px", marginBottom: "12px" }} ><TouchAppIcon sx={{ color: "#636161", marginRight: "10px" }} /><Typography variant='p'>{dataset?.text}</Typography></div>}<Divider></Divider>
+                        {dataset?.result_type === "info" && <div style={{ marginTop: "12px", marginBottom: "12px" }} ><InfoIcon sx={{ color: "#636161", marginRight: "10px" }} /><Typography variant='p'>{dataset?.text}</Typography></div>}<Divider></Divider>
                     </Grid>
                     <Grid >
                         {dataset?.result_type === "fail" && <div style={{ marginTop: "12px", marginBottom: "12px" }} >{dataset?.result_type.includes("fail") ? <CancelIcon sx={{ color: "red", marginRight: "10px" }} /> : <CheckCircleIcon sx={{ color: "green", marginRight: "10px" }} />}<Typography variant='p'>{dataset?.text}</Typography></div>}
