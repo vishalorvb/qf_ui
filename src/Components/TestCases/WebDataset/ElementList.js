2c51f3d0b6760e1fc6f2e143c91f4e4c60006fd3
Your Name <you@example.com>
2023-06-16 11:32:07 +0530

    minor change

--- src/Components/TestCases/WebDataset/ElementList.js
+++ src/Components/TestCases/WebDataset/ElementList.js
@@ -82,7 +82,7 @@ function ElementList({ elementList, updateDataset, screenName }) {
                                 defaultValue={param.row.dataset_values.input_value}
                                 onKeyDown={(event) => {
                                     event.stopPropagation();
-                                  }}
+                                }}
                                 onChange={(e) => {
                                     updateDataset(
                                         param.row.element_id,
@@ -108,6 +108,7 @@ function ElementList({ elementList, updateDataset, screenName }) {
                         )}
                         {param.row.web_page_elements.input_type == "Link" && (
                             <select
+                                defaultValue={param.row.dataset_values.input_value}
                                 onChange={(e) => {
                                     updateDataset(
                                         param.row.element_id,
@@ -175,9 +176,9 @@ function ElementList({ elementList, updateDataset, screenName }) {
                         </div>
                         {inputList.includes(param.row.element_id) && <div style={{ marginTop: "10px" }}>
                             <input
-                              onKeyDown={(event) => {
-                                event.stopPropagation();
-                              }}
+                                onKeyDown={(event) => {
+                                    event.stopPropagation();
+                                }}
                                 onChange={e => {
                                     updateDataset(param.row.element_id, "validate_text", e.target.value.trim())
                                 }}
