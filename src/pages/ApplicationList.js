745e20577f6bdc2b9cf0b0786222bd90ed8410e0
Abhishek Choudhary <abhishek.choudhary@prolifics.com>
2023-08-23 14:52:27 +0530

    submodule ui updated

--- src/pages/ApplicationList.js
+++ src/pages/ApplicationList.js
@@ -1,29 +1,22 @@
 import { useEffect, useState } from "react";
 import useHead from "../hooks/useHead";
 import Table from "../CustomComponent/Table";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-import CreateApplication from "../Components/Application/CreateApplication";
 import SnackbarNotify from "../CustomComponent/SnackbarNotify";
 import { getApplication } from "../Services/ApplicationService";
-import { ApplicationNav } from "../Components/Application/ApplicationNav";
 import {
   Button,
   IconButton,
-  Menu,
   MenuItem,
-  Popover,
   Select,
   Tooltip,
   Typography,
 } from "@mui/material";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import { moduledata } from "../Components/Application/CreateApplication";
-import ScreenshotMonitorIcon from "@mui/icons-material/ScreenshotMonitor";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import { deleteApplication } from "../Services/ApplicationService";
-import AddIcon from "@mui/icons-material/Add";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Stack } from "@mui/system";
 import LanguageIcon from "@mui/icons-material/Language";
 import AndroidIcon from "@mui/icons-material/Android";
@@ -32,16 +25,32 @@ import AdUnitsIcon from "@mui/icons-material/AdUnits";
 import ApiIcon from "@mui/icons-material/Api";
 import JoinInnerRoundedIcon from "@mui/icons-material/JoinInnerRounded";
 import ConfirmPop from "../CustomComponent/ConfirmPop";
+import TableActions from "../CustomComponent/TableActions";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+
 export default function ApplicationsList() {
-  const { setHeader } = useHead();
+  const { setHeader, setSnackbarData } = useHead();
   const { auth } = useAuth();
   const navigate = useNavigate();
   const [application, setApplication] = useState([]);
   const location = useLocation();
   const [snack, setSnack] = useState(false);
   let [snackbarsuccess, setSnackbarsuccess] = useState(false);
+  let [popup, setPopup] = useState(false);
 
-  const handleDelete = () => {};
+  const handledeleteApplication = (module_id, id) => {
+    deleteApplication(module_id, id).then((res) => {
+      if (res) {
+        getApplication(setApplication, auth?.info?.id);
+        setSnackbarData({
+          status: true,
+          message: "Application Deleted Successfully",
+          severity: "success",
+        });
+      }
+    });
+    setPopup(false);
+  };
 
   const applicationColumns = [
     {
@@ -107,49 +116,52 @@ export default function ApplicationsList() {
     {
       field: "sub_modules_list",
       headerName: "Sub Applications",
-      flex: 1,
+      flex: 2,
       renderCell: (param) => {
-        // return <div>abhi</div>;
         const subModules = param?.row?.sub_modules_list;
         const createSubModule = (
-          <Button
-            variant="contained"
-            onClick={() => {
-              moduledata.module_name = param.row.module_name;
-              moduledata.base_url = param.row.base_url;
-              moduledata.module_desc = param.row.module_desc;
-              moduledata.module_type = param.row.module_type;
-              moduledata.is_sub_module = true;
-              moduledata.parent_module_id = param.row.module_id;
-              navigate("CreateSubApplication", { state: param?.row });
-            }}
-          >
-            create submodule
-          </Button>
+          <Tooltip title="Create SubModule">
+            <IconButton
+              variant="contained"
+              size="small"
+              onClick={() => {
+                moduledata.module_name = param.row.module_name;
+                moduledata.base_url = param.row.base_url;
+                moduledata.module_desc = param.row.module_desc;
+                moduledata.module_type = param.row.module_type;
+                moduledata.is_sub_module = true;
+                moduledata.parent_module_id = param.row.module_id;
+                navigate("CreateSubApplication", { state: param?.row });
+              }}
+            >
+              <AddCircleOutlineIcon color="primary" />
+            </IconButton>
+          </Tooltip>
         );
         return subModules?.length > 0 ? (
-          <Select fullWidth size="small">
-            {subModules?.map((module) => (
-              <MenuItem
-                onClick={() => {
-                  if (module.module_type == "19") {
-                    setSnack(true);
-                    setTimeout(() => {
-                      setSnack(false);
-                    }, 3000);
-                  } else {
-                    navigate(`${module}`, {
-                      state: module,
-                    });
-                  }
-                }}
-              >
-                {module?.module_name}
-              </MenuItem>
-            ))}
-
+          <>
             {createSubModule}
-          </Select>
+            <Select fullWidth size="small">
+              {subModules?.map((module) => (
+                <MenuItem
+                  onClick={() => {
+                    if (module.module_type == "19") {
+                      setSnack(true);
+                      setTimeout(() => {
+                        setSnack(false);
+                      }, 3000);
+                    } else {
+                      navigate(`${module}`, {
+                        state: module,
+                      });
+                    }
+                  }}
+                >
+                  {module?.module_name}
+                </MenuItem>
+              ))}
+            </Select>
+          </>
         ) : (
           createSubModule
         );
@@ -161,15 +173,31 @@ export default function ApplicationsList() {
       flex: 6,
       sortable: false,
       renderCell: (param) => {
-        return ApplicationDescriptionCell(
-          param,
-          setApplication,
-          auth,
-          setSnackbarsuccess
+        return (
+          <TableActions heading={param?.row?.module_desc}>
+            <MenuItem
+              onClick={() => {
+                moduledata.module_name = param.row.module_name;
+                moduledata.base_url = param.row.base_url;
+                moduledata.module_desc = param.row.module_desc;
+                moduledata.module_type = param.row.module_type;
+                moduledata.module_id = param.row.module_id;
+                navigate("Update", { state: param?.row });
+              }}
+            >
+              <EditOutlinedIcon sx={{ color: "blue", mr: 1 }} />
+              Edit
+            </MenuItem>
+            <MenuItem onClick={() => setPopup(true)}>
+              <DeleteOutlineIcon sx={{ color: "red", mr: 1 }} />
+              Delete
+            </MenuItem>
+          </TableActions>
         );
       },
     },
   ];
+
   useEffect(() => {
     setHeader((ps) => {
       return {
@@ -207,7 +235,7 @@ export default function ApplicationsList() {
         columns={applicationColumns}
         getRowId={(row) => row?.module_id}
         searchPlaceholder="Search Applications"
-        rowHeight={60}
+        rowHeight={65}
       />
       {snack && (
         <SnackbarNotify
@@ -223,83 +251,13 @@ export default function ApplicationsList() {
         msg="Application deleted succesfully"
         severity="success"
       />
-    </div>
-  );
-}
-
-const ApplicationDescriptionCell = (
-  param,
-  setApplication,
-  auth,
-  setSnackbarsuccess
-) => {
-  const navigate = useNavigate();
-  let [popup, setPopup] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const handledeleteApplication = (module_id, id) => {
-    deleteApplication(module_id, id).then((res) => {
-      if (res) {
-        getApplication(setApplication, auth?.info?.id);
-        setSnackbarsuccess(true);
-      }
-    });
-    setPopup(false);
-  };
-  return (
-    <div className="descColumn">
-      <Typography variant="p">{param?.row?.module_desc}</Typography>
-      <MoreVertIcon
-        id="basic-button"
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-        className="descOption"
-      />
-
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        <MenuItem
-          onClick={() => {
-            moduledata.module_name = param.row.module_name;
-            moduledata.base_url = param.row.base_url;
-            moduledata.module_desc = param.row.module_desc;
-            moduledata.module_type = param.row.module_type;
-            moduledata.module_id = param.row.module_id;
-            navigate("Update", { state: param?.row });
-          }}
-        >
-          <EditOutlinedIcon sx={{ color: "blue", mr: 1 }} />
-          Edit
-        </MenuItem>
-        <MenuItem onClick={() => setPopup(true)}>
-          <DeleteOutlineIcon sx={{ color: "red", mr: 1 }} />
-          Delete
-        </MenuItem>
-      </Menu>
       <ConfirmPop
         open={popup}
         handleClose={() => setPopup(false)}
         heading={"Delete Application"}
         message={"Are you sure you want to delete this application"}
-        onConfirm={() =>
-          handledeleteApplication(param?.row?.module_id, auth?.info?.id)
-        }
+        onConfirm={() => handledeleteApplication(1, auth?.info?.id)}
       ></ConfirmPop>
     </div>
   );
-};
+}
