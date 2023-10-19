cd4898557e97fe05b7be63a2920db1bda9ef598b
Abhishek Choudhary <abhishek.choudhary@prolifics.com>
2023-10-19 14:52:43 +0530

    ts dependency removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: "",
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react()],
   server: {
     // this ensures that the browser opens upon server start
     open: true,
