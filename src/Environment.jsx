4f94e8d62012493a1881ca4ad9a2c501c8067315
Abhishek Choudhary <abhishek.choudhary@prolifics.com>
2023-12-07 10:49:13 +0530

    userexecutionparams

--- src/Environment.jsx
+++ src/Environment.jsx
@@ -12,4 +12,4 @@ export let report = "http://10.11.12.243:8065" + "/qfreportservice";
 export let biservice = "http://10.11.12.243:8066" + "/biservice";
 
 export let SAVE_USER_LOCAL_GIT_DETAILS =
-  "http://localhost:8062/qfuserservice/userexecutionparameters/saveUserExecutionParameters";
+  "http://10.11.12.243:8062/qfuserservice/userexecutionparameters/saveUserExecutionParameters";
