511b92dc637051ca22bed74219c5ab3a5dd9784a
Your Name <you@example.com>
2023-12-11 10:26:39 +0530

    error fixed

--- src/Components/TestLibrary/TestLibraryService.jsx
+++ src/Components/TestLibrary/TestLibraryService.jsx
@@ -2,81 +2,81 @@ import axios from "axios";
 import { qfservice } from "../../Environment";
 
 export function GetSprintsOfJiraProject(callback, userId, projectId) {
-  axios
-    .postForm(
-      `${qfservice}/getSprintsOfJiraProject?user_id=${userId}&project_id=${projectId}`
-    )
-    .then((resp) => {
-      callback(resp?.data?.info);
-    });
+    projectId != undefined && axios
+        .postForm(
+            `${qfservice}/getSprintsOfJiraProject?user_id=${userId}&project_id=${projectId}`
+        )
+        .then((resp) => {
+            callback(resp?.data?.info);
+        });
 }
 
 export function GetIssuesOfTestlibrary(callback, sprintName, key, projectId) {
-  axios
-    .postForm(
-      `${qfservice}/getIssuesOfTestlibrary?sprint_name=${sprintName}&key=${key}&project_id=${projectId}`
-    )
-    .then((resp) => {
-      callback(resp);
-    });
+    projectId != undefined && axios
+        .postForm(
+            `${qfservice}/getIssuesOfTestlibrary?sprint_name=${sprintName}&key=${key}&project_id=${projectId}`
+        )
+        .then((resp) => {
+            callback(resp);
+        });
 }
 
 export function GetTestLibrary(
-  callback,
-  projectId,
-  userId,
-  projectKey,
-  sprintName,
-  issueKey
+    callback,
+    projectId,
+    userId,
+    projectKey,
+    sprintName,
+    issueKey
 ) {
-  let data = {
-    project_key: projectKey,
-    sprint_name: sprintName,
-    issue_key: issueKey,
-  };
-  axios
-    .post(
-      `${qfservice}/getTestLibrary?project_id=${projectId}&user_id=${userId}`,
-      data
-    )
-    .then((resp) => {
-      callback(resp?.data?.info);
-    });
+    let data = {
+        project_key: projectKey,
+        sprint_name: sprintName,
+        issue_key: issueKey,
+    };
+    projectId != undefined && axios
+        .post(
+            `${qfservice}/getTestLibrary?project_id=${projectId}&user_id=${userId}`,
+            data
+        )
+        .then((resp) => {
+            callback(resp?.data?.info);
+        });
 }
 export function ExportManualTestcases(
-  callback,
-  projectId,
-  userId,
-  projectKey,
-  sprintKey,
-  issueKey
+    callback,
+    projectId,
+    userId,
+    projectKey,
+    sprintKey,
+    issueKey
 ) {
-  axios
-    .get(
-      `${qfservice}/export-manual-testcases/${projectKey}/${sprintKey}/${issueKey}?project_id=${projectId}&user_id=${userId}`
-    )
-    .then((resp) => {
-      callback(resp?.data?.info);
-    });
+    projectId != undefined && axios
+        .get(
+            `${qfservice}/export-manual-testcases/${projectKey}/${sprintKey}/${issueKey}?project_id=${projectId}&user_id=${userId}`
+        )
+        .then((resp) => {
+            callback(resp?.data?.info);
+        });
 }
 
 export function UploadManualTestcasesExcelFile(callback, postData) {
-  axios
-    .postForm(`${qfservice}/uploadManualTestcasesExcelFile`, postData)
-    .then((resp) => {
-      console.log(resp);
-      callback(resp);
-    });
+    axios
+        .postForm(`${qfservice}/uploadManualTestcasesExcelFile`, postData)
+        .then((resp) => {
+            console.log(resp);
+            callback(resp);
+        });
 }
 
 export async function DeleteManualTestcase(testcaseId) {
-  let x = await axios
-    .post(`${qfservice}/deleteManualTestcase?manual_testcase_id=${testcaseId}`)
-    .then((res) => {
-      if (res.data.status === "SUCCESS") {
-        return true;
-      }
-      return false;
-    });
-  return x;
+    let x = await axios
+        .post(`${qfservice}/deleteManualTestcase?manual_testcase_id=${testcaseId}`)
+        .then((res) => {
+            if (res.data.status === "SUCCESS") {
+                return true;
+            }
+            return false;
+        });
+    return x;
 }
