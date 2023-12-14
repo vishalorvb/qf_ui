f293181466b36bf14c248e09e8a60a052cf318f9
Your Name <you@example.com>
2023-12-14 10:11:40 +0530

    url name changed

--- docker/dev/pipeline.sh
+++ docker/dev/pipeline.sh
@@ -16,12 +16,12 @@ sudo docker build -t reactcicdimage:latest .
 
 sudo docker run \
 -e REACT_APP_BASE_URL='https://api.example.com' \
--e REACT_APP_authservice='http://10.11.12.243:8061' \
--e REACT_APP_userservice='http://10.11.12.243:8062/qfuserservice' \
--e REACT_APP_qfservice='http://10.11.12.243:8063/qfservice' \
--e REACT_APP_dashboard='http://10.11.12.243:8064/qfdashboard' \
--e REACT_APP_report='http://10.11.12.243:8065/qfreportservice' \
--e REACT_APP_biservice='http://10.11.12.243:8066/bireport' \
+-e REACT_APP_AUTH_SERVICE='http://10.11.12.243:8061' \
+-e REACT_APP_USER_SERVICE='http://10.11.12.243:8062/qfuserservice' \
+-e REACT_APP_APP_SERVICE='http://10.11.12.243:8063/qfservice' \
+-e REACT_APP_DASHBOARD_SERVICE='http://10.11.12.243:8064/qfdashboard' \
+-e REACT_APP_REPORT_SERVICE='http://10.11.12.243:8065/qfreportservice' \
+-e REACT_APP_BI_SERVICE='http://10.11.12.243:8066/bireport' \
 -e REACT_APP_SAVE_USER_LOCAL_GIT_DETAILS='http://10.11.12.243:8062/qfuserservice/userexecutionparameters/saveUserExecutionParameters' \
 -p 3000:3000 -d --name reactcicdcontainer reactcicdimage:latest
 
--- src/Environment.jsx
+++ src/Environment.jsx
@@ -1,14 +1,14 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL
-export let authservice = process.env.REACT_APP_authservice;
+export let authservice = process.env.REACT_APP_AUTH_SERVICE;
 
-export let userservice = process.env.REACT_APP_userservice
+export let userservice = process.env.REACT_APP_USER_SERVICE
 
-export let qfservice = process.env.REACT_APP_qfservice
+export let qfservice = process.env.REACT_APP_APP_SERVICE
 
-export let dashboard = process.env.REACT_APP_dashboard
+export let dashboard = process.env.REACT_APP_DASHBOARD_SERVICE
 
-export let report = process.env.REACT_APP_report
+export let report = process.env.REACT_APP_REPORT_SERVICE
 
-export let biservice = process.env.REACT_APP_biservice
+export let biservice = process.env.REACT_APP_BI_SERVICE
 
 export let SAVE_USER_LOCAL_GIT_DETAILS = process.env.REACT_APP_SAVE_USER_LOCAL_GIT_DETAILS
