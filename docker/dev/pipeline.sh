c87b2109cc3282aa4f09012f1a9ebf267153d8d8
Your Name <you@example.com>
2023-12-14 15:16:52 +0530

    url modified

--- docker/dev/pipeline.sh
+++ docker/dev/pipeline.sh
@@ -15,14 +15,14 @@ sudo docker build -t reactcicdimage:latest .
 
 
 sudo docker run \
--e REACT_APP_BASE_URL='https://api.example.com' \
--e REACT_APP_AUTH_SERVICE='http://10.11.12.243:8061' \
--e REACT_APP_USER_SERVICE='http://10.11.12.243:8062/qfuserservice' \
--e REACT_APP_APP_SERVICE='http://10.11.12.243:8063/qfservice' \
--e REACT_APP_DASHBOARD_SERVICE='http://10.11.12.243:8064/qfdashboard' \
--e REACT_APP_REPORT_SERVICE='http://10.11.12.243:8065/qfreportservice' \
--e REACT_APP_BI_SERVICE='http://10.11.12.243:8066/bireport' \
--e REACT_APP_SAVE_USER_LOCAL_GIT_DETAILS='http://10.11.12.243:8062/qfuserservice/userexecutionparameters/saveUserExecutionParameters' \
+-e REACT_APP_BASE_URL='http://10.11.12.243:8083' \
+-e REACT_APP_AUTH_SERVICE='http://10.11.12.243:8051/qfauthservice' \
+-e REACT_APP_USER_SERVICE='http://10.11.12.243:8052/qfuserservice' \
+-e REACT_APP_APP_SERVICE='http://10.11.12.243:8053/qfservice' \
+-e REACT_APP_DASHBOARD_SERVICE='http://10.11.12.243:8054/qfdashboard' \
+-e REACT_APP_REPORT_SERVICE='http://10.11.12.243:8055/qfreportservice' \
+-e REACT_APP_BI_SERVICE='http://10.11.12.243:8056/biservice' \
+-e REACT_APP_SAVE_USER_LOCAL_GIT_DETAILS='http://10.11.12.243:8052/qfuserservice/userexecutionparameters/saveUserExecutionParameters' \
 -p 3000:3000 -d --name reactcicdcontainer reactcicdimage:latest
 
 exit
