7d13504c220cf5e69d14ad00ed7953add753a8cd
Abhishek Choudhary <abhishek.choudhary@prolifics.com>
2024-01-17 18:32:30 +0530

    heading fixed

--- src/CustomComponent/MastPop.jsx
+++ src/CustomComponent/MastPop.jsx
@@ -1,23 +1,22 @@
-import { Dialog, IconButton } from "@mui/material";
+import { Dialog, DialogContent, DialogTitle, IconButton } from "@mui/material";
 import React from "react";
 import CloseIcon from "@mui/icons-material/Close";
 
 function MastPop(props) {
   return (
     <Dialog open={props.open} maxWidth="md">
-      <div
-        style={{ backgroundColor: "#009fee", padding: "7px", color: "white" }}
-      >
-        <h2>
-          {props.heading}
-          <span>
-            <IconButton sx={{ float: "right" }} onClick={e=>props.setOpen(false)}>
-              <CloseIcon></CloseIcon>
-            </IconButton>
-          </span>
-        </h2>
-      </div>
-      <div style={{ padding: "10px" }}>{props.children}</div>
+      <DialogTitle className="dialogTitle">
+        {props.heading}
+        <span>
+          <IconButton
+            sx={{ float: "right" }}
+            onClick={(e) => props.setOpen(false)}
+          >
+            <CloseIcon></CloseIcon>
+          </IconButton>
+        </span>
+      </DialogTitle>
+      <DialogContent>{props.children}</DialogContent>
     </Dialog>
   );
 }
