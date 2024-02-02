a0b665da8805f33a26b5e84208fab0c8342ef25c
Your Name <you@example.com>
2024-02-02 11:48:48 +0530

    userinfo api issue fixed

--- src/Layout/Login.jsx
+++ src/Layout/Login.jsx
@@ -8,209 +8,209 @@ import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import useAuth from "../hooks/useAuth";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import axios from "../Services/AxiosConfig";
 import { useEffect } from "react";
 import { authservice } from "../Environment";
 import SnackbarNotify from "../CustomComponent/SnackbarNotify";
 import { Alert } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
-
+import { AxiosWithoutHeader } from "../utilities/Utility";
 export function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" to="//https://prolifics.com/">
-        Prolifics
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
+    return (
+        <Typography
+            variant="body2"
+            color="text.secondary"
+            align="center"
+            {...props}
+        >
+            {"Copyright © "}
+            <Link color="inherit" to="//https://prolifics.com/">
+                Prolifics
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
 }
 
 const theme = createTheme();
 
 export default function Login() {
-  const [loginErr, setLoginErr] = React.useState(false);
-  const [fieldsErr, setfieldsErr] = React.useState({ email: "", password: "" });
-  const [loading, setLoading] = React.useState(false);
-  const { setAuth, auth } = useAuth();
-  const navigate = useNavigate();
+    console.log("login")
+    const [loginErr, setLoginErr] = React.useState(false);
+    const [fieldsErr, setfieldsErr] = React.useState({ email: "", password: "" });
+    const [loading, setLoading] = React.useState(false);
+    const { setAuth, auth } = useAuth();
+    const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    setfieldsErr({ email: "", password: "" });
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const email = data.get("email").trim();
-    const password = data.get("password").trim();
-    if (email && password) {
-      setLoading(true);
-      try {
-        const response = await axios.post(
-          authservice + "/authentication/login",
-          {
-            username: data.get("email"),
-            password: data.get("password"),
-          },
-          {
-            headers: {
-              Authorization: "Basic c2FuanU6ZGV2cmFiYml0",
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        const token = response?.data?.token;
-        localStorage.setItem("token", token);
+    const handleSubmit = async (event) => {
+        setfieldsErr({ email: "", password: "" });
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const email = data.get("email").trim();
+        const password = data.get("password").trim();
+        if (email && password) {
+            setLoading(true);
+            try {
+                const response = await AxiosWithoutHeader.post(
+                    authservice + "/authentication/login",
+                    {
+                        username: data.get("email"),
+                        password: data.get("password"),
+                    },
+                    //{
+                    //    headers: {
+                    //        Authorization: "Basic c2FuanU6ZGV2cmFiYml0",
+                    //        "Content-Type": "application/json",
+                    //    },
+                    //}
+                );
+                const token = response?.data?.token;
+                localStorage.setItem("token", token);
+                console.log(token)
+                const userInfo = await axios.get(
+                    authservice + "/authentication/userInfo",
+                    {
+                        //headers: {
+                        //    "Content-Type": "application/json",
+                        //    Authorization: `Bearer ${token}`,
+                        //},
+                    }
+                );
+                const info = userInfo?.data?.info;
+                const user = info?.ssoId;
+                const password = info?.password;
+                const role = info?.role;
+                const userId = info?.id;
 
-        const userInfo = await axios.get(
-          authservice + "/authentication/userInfo",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        const info = userInfo?.data?.info;
-        const user = info?.ssoId;
-        const password = info?.password;
-        const role = info?.role;
-        const userId = info?.id;
+                setAuth({
+                    user: user,
+                    password: password,
+                    roles: role,
+                    userId: userId,
+                    info: info,
+                    token: token,
+                });
+                setLoading(false);
+            } catch (err) {
+                const error = err?.response?.data;
+                setLoginErr({
+                    state: true,
+                    message: error ? error?.message : "Network Error !!",
+                    status: error?.status,
+                });
+            }
+            setLoading(false);
+        } else {
+            !email &&
+                setfieldsErr((ps) => {
+                    return { ...ps, email: "username is required" };
+                });
+            !password &&
+                setfieldsErr((ps) => {
+                    return { ...ps, password: "password is required" };
+                });
+        }
+    };
 
-        setAuth({
-          user: user,
-          password: password,
-          roles: role,
-          userId: userId,
-          info: info,
-          token: token,
-        });
-        setLoading(false);
-      } catch (err) {
-        console.log(err?.response?.data);
-        const error = err?.response?.data;
+    useEffect(() => {
+        // auth?.user && navigate(from, { replace: true });
+        auth?.user &&
+            auth?.roles &&
+            navigate("/Favourite-Projects", { state: "recentProjects" });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [auth]);
 
-        setLoginErr({
-          state: true,
-          message: error ? error?.message : "Network Error !!",
-          status: error?.status,
-        });
-      }
-      setLoading(false);
-    } else {
-      !email &&
-        setfieldsErr((ps) => {
-          return { ...ps, email: "username is required" };
-        });
-      !password &&
-        setfieldsErr((ps) => {
-          return { ...ps, password: "password is required" };
-        });
-    }
-  };
+    useEffect(() => {
+        // auth?.user && navigate(from, { replace: true });
+        auth?.user &&
+            auth?.roles &&
+            navigate("/Favourite-Projects", { state: "recentProjects" });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  useEffect(() => {
-    // auth?.user && navigate(from, { replace: true });
-    auth?.user &&
-      auth?.roles &&
-      navigate("/Favourite-Projects", { state: "recentProjects" });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [auth]);
+    return (
+        <ThemeProvider theme={theme}>
+            <SnackbarNotify
+                open={loginErr?.state}
+                close={setLoginErr}
+                msg={loginErr?.message}
+                severity="error"
+            />
+            <Grid container component="main" sx={{ height: "100vh" }}>
+                <CssBaseline />
+                <Grid item xs={false} sm={12} md={7} className="loginImg">
+                    <img src="logo-light.png" className="loginLogo" alt="" />
+                </Grid>
+                <Grid
+                    item
+                    xs={12}
+                    sm={12}
+                    md={5}
+                    component={Paper}
+                    className="loginForm"
+                    square
+                >
+                    <Box>
+                        <Typography component="h1" variant="h3">
+                            Welcome back
+                        </Typography>
+                        <br />
+                        <Typography component="h1" variant="subtitle1">
+                            Please enter Your Credentials
+                        </Typography>
+                        <Box
+                            component="form"
+                            noValidate
+                            onSubmit={handleSubmit}
+                            sx={{ mt: 1 }}
+                        >
+                            {loginErr?.status === 401 && (
+                                <Alert severity="warning">Credentials are wrong !!</Alert>
+                            )}
+                            <TextField
+                                margin="normal"
+                                size="small"
+                                required
+                                fullWidth
+                                id="email"
+                                label="UserId"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                error={fieldsErr?.email || loginErr?.status === 401}
+                                helperText={fieldsErr?.email}
+                            />
+                            <TextField
+                                margin="normal"
+                                size="small"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                error={fieldsErr?.password || loginErr?.status === 401}
+                                helperText={fieldsErr?.password}
+                            />
 
-  useEffect(() => {
-    // auth?.user && navigate(from, { replace: true });
-    auth?.user &&
-      auth?.roles &&
-      navigate("/Favourite-Projects", { state: "recentProjects" });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+                            <LoadingButton
+                                type="submit"
+                                size="small"
+                                fullWidth
+                                variant="contained"
+                                loading={loading}
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign In
+                            </LoadingButton>
 
-  return (
-    <ThemeProvider theme={theme}>
-      <SnackbarNotify
-        open={loginErr?.state}
-        close={setLoginErr}
-        msg={loginErr?.message}
-        severity="error"
-      />
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
-        <Grid item xs={false} sm={12} md={7} className="loginImg">
-          <img src="logo-light.png" className="loginLogo" alt="" />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          md={5}
-          component={Paper}
-          className="loginForm"
-          square
-        >
-          <Box>
-            <Typography component="h1" variant="h3">
-              Welcome back
-            </Typography>
-            <br />
-            <Typography component="h1" variant="subtitle1">
-              Please enter Your Credentials
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
-            >
-              {loginErr?.status === 401 && (
-                <Alert severity="warning">Credentials are wrong !!</Alert>
-              )}
-              <TextField
-                margin="normal"
-                size="small"
-                required
-                fullWidth
-                id="email"
-                label="UserId"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                error={fieldsErr?.email || loginErr?.status === 401}
-                helperText={fieldsErr?.email}
-              />
-              <TextField
-                margin="normal"
-                size="small"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                error={fieldsErr?.password || loginErr?.status === 401}
-                helperText={fieldsErr?.password}
-              />
-
-              <LoadingButton
-                type="submit"
-                size="small"
-                fullWidth
-                variant="contained"
-                loading={loading}
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </LoadingButton>
-
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
-  );
+                            <Copyright sx={{ mt: 5 }} />
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
+    );
 }
--- src/Layout/PersistLogin.jsx
+++ src/Layout/PersistLogin.jsx
@@ -3,50 +3,52 @@ import { useState, useEffect } from "react";
 import useAuth from "../hooks/useAuth";
 import PersistingLoading from "./PersistingLoading";
 import { authservice } from "../Environment";
+import { AxiosWithoutHeader } from "../utilities/Utility";
 import axios from "axios";
 
 const PersistLogin = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const { auth, setAuth } = useAuth();
+    console.log("persist login")
+    const [isLoading, setIsLoading] = useState(true);
+    const { auth, setAuth } = useAuth();
 
-  const token = auth?.token ? auth?.token : localStorage.getItem("token");
+    const token = auth?.token ? auth?.token : localStorage.getItem("token");
 
-  useEffect(() => {
-    const verifyUser = async () => {
-      try {
-        const userInfo = await axios.get(
-          authservice + "/authentication/userInfo",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        const info = userInfo?.data?.info;
-        const user = info?.ssoId;
-        const password = info?.password;
-        const role = info?.role;
-        const userId = info?.id;
-        setAuth({
-          user: user,
-          password: password,
-          roles: role,
-          userId: userId,
-          info: info,
-          token: token,
-        });
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+    useEffect(() => {
+        const verifyUser = async () => {
+            try {
+                const userInfo = await axios.get(
+                    authservice + "/authentication/userInfo",
+                    //{
+                    //    headers: {
+                    //        Authorization: `Bearer ${token}`,
+                    //    },
+                    //}
+                );
+                const info = userInfo?.data?.info;
+                const user = info?.ssoId;
+                const password = info?.password;
+                const role = info?.role;
+                const userId = info?.id;
+                setAuth({
+                    user: user,
+                    password: password,
+                    roles: role,
+                    userId: userId,
+                    info: info,
+                    token: token,
+                });
+            } catch (error) {
 
-    !auth?.user ? verifyUser() : setIsLoading(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+            } finally {
+                setIsLoading(false);
+            }
+        };
 
-  return <>{isLoading ? <PersistingLoading /> : <Outlet />}</>;
+        !auth?.user ? verifyUser() : setIsLoading(false);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return <>{isLoading ? <PersistingLoading /> : <Outlet />}</>;
 };
 
 export default PersistLogin;
